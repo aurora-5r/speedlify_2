d33a44dda4382da16f2a26d01bde84e45a6854fd
Laurent Maumet <laurent.maumet@gmail.com>
2022-02-17 16:55:32 +0100

    Update uxium.js

--- _data/sites/uxium.js
+++ _data/sites/uxium.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "uxium", 
 	description: "The uxium sites",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 1, // (in minutes)
 	},
 	urls: [
 		"https://uxium.io",
