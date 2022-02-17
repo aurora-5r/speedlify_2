f6bdbd218505be18d77122a0d91353d4fba5fad3
Laurent Maumet <laurent.maumet@gmail.com>
2022-02-17 16:54:09 +0100

    Update uxium.js

--- _data/sites/uxium.js
+++ _data/sites/uxium.js
@@ -5,6 +5,11 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://uxium.io"
+		"https://uxium.io",
+		"https://aurora-5r.fr/",
+        "https://xposur.aurora-5r.fr/",
+        "https://www.scierie-borie.fr/",
+        "https://cedric.uxim.io/",
+        "https://bikeslot.uxim.io/"
 	]
 };
