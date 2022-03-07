9e6b207ddd62d2e6db1f3859dfdf5925ea9e2fc9
Laurent Maumet <laurent.maumet@gmail.com>
2022-03-07 08:53:39 +0100

    Create in-progress.js

---
+++ _data/sites/in-progress.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "uxium", 
+	description: "The sites in progress for short interval tests",
+	options: {
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+	"https://ux-ium.web.app/"
+	]
+};
